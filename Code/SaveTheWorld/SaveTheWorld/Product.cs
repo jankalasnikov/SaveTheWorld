d104b675956a4ab796c84a9b6ee30f048c1f1345
DenisKovacek <54936220+DenisKovacek@users.noreply.github.com>
2019-10-22 09:16:12 +0200

    Create Product.cs
    product class created

---
+++ Code/SaveTheWorld/SaveTheWorld/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveTheWorld
+{
+    class Product
+    {
+        private String name { get; set; }
+        private double price { get; set; }
+        private String itemDescription { get; set; }
+        private int stock { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(String name, double price, String itemDescription, int stock)
+        {
+            this.name = name;
+            this.price = price;
+            this.itemDescription = itemDescription;
+            this.stock = stock;
+        }
+    }
+}
