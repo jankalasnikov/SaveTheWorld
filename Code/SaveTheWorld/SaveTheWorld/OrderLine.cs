68224295e459ecda67949cad9428d221a7ed72aa
Jan <jan.kalasnikov@seznam.cz>
2019-10-22 10:27:16 +0200

    OrderLine
    created orderLine class

---
+++ Code/SaveTheWorld/SaveTheWorld/OrderLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace SaveTheWorld
+{
+    class OrderLine
+    {
+        private int quantity { get; set; }
+
+        public OrderLine()
+        {
+            
+        }
+
+        public OrderLine(int quantity)
+        {
+            this.quantity = quantity; 
+        }
+    }
+}
