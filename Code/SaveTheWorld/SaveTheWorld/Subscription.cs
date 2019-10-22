e4b03f3575bc4b003f2b2047d2409a8a236107d7
DenisKovacek <54936220+DenisKovacek@users.noreply.github.com>
2019-10-22 09:12:49 +0200

    Create Subscription.cs
    subscription class created

---
+++ Code/SaveTheWorld/SaveTheWorld/Subscription.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveTheWorld
+{
+    class Subscription
+    {
+        private int periodOfTime { get; set; }
+        private String typeOfSubscription { get; set; }
+        private int amount { get; set; }
+        private String startDate { get; set; }
+
+        public Subscription()
+        {
+
+        }
+
+        public Subscription(int periodOfTime, String typeOfSubscription, int amount, String startDate)
+        {
+            this.periodOfTime = periodOfTime;
+            this.typeOfSubscription = typeOfSubscription;
+            this.amount = amount;
+            this.startDate = startDate;
+        }
+    }
+}
