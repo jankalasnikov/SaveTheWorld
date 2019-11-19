1ea2e768524991a6a6727a51201ecd12ddd56c41
DenisKovacek <54936220+DenisKovacek@users.noreply.github.com>
2019-11-19 12:08:32 +0100

    product ctr

--- SaveWorld/SaveWorldController/ProductCtr.cs
+++ SaveWorld/SaveWorldController/ProductCtr.cs
@@ -31,5 +31,100 @@ namespace SaveWorldController
             }
             return prodData;
         }
+        public void AddProduct(int productId, string name, double price, string productDescription, int stock)
+        {
+
+            using (SaveWorldEntities dbEntities = new SaveWorldEntities())
+
+            {
+                try
+                {
+                    aproduct product = new aproduct()
+                    {
+
+                        name = name,
+                        price = price,
+                        productDescription = productDescription,
+                        stock = stock,
+                    };
+
+                    dbEntities.Aproduct.Add(product);
+                    dbEntities.SaveChanges();
+
+
+                }
+                catch (DbEntityValidationException e)
+                {
+                    foreach (var eve in e.EntityValidationErrors)
+                    {
+                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        foreach (var ve in eve.ValidationErrors)
+                        {
+                            Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                                ve.PropertyName, ve.ErrorMessage);
+                        }
+                    }
+                    throw;
+                }
+            }
+        }
+        public void CreateProduct(Product newProduct)
+        {
+
+            using (SaveWorldEntities dbEntities = new SaveWorldEntities())
+
+            {
+
+                aproduct product = new aproduct()
+                {
+
+                    name = newProduct.Name,
+                    price = newProduct.Price,
+                    productDescription = newProduct.ProductDescription,
+                    stock = newProduct.Stock,
+                };
+
+                dbEntities.Aproduct.Add(product);
+
+                try
+                {
+                    dbEntities.SaveChanges();
+                }
+                catch (DbEntityValidationException e)
+                {
+                    foreach (var eve in e.EntityValidationErrors)
+                    {
+                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        foreach (var ve in eve.ValidationErrors)
+                        {
+                            Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                                ve.PropertyName, ve.ErrorMessage);
+                        }
+                    }
+
+                }
+            }
+        }
+
+         public Product DeleteProduct(int id)
+        {
+            using (var NWEntities = new SaveWorldEntities())
+            {
+                var product = (from p in NWEntities.Products
+                               where p.id == id
+                               select p).FirstOrDefault();
+                if (product != null)
+                    //prodData = delete Product()
+                    {
+                   
+                    dbEntities.Aproduct.Remove(product);
+                    dbEntities.SaveChanges();
+                };
+            }
+        }
+
+            
     }
 }
